3d298247abf2d36f8607eddabf83cb3ab7403ec1
Niraj Kumar <nirajk23523@gmail.com>
2025-10-30 21:41:01 +0530

    feat: integrate Material3 theming and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
+import { useMemo } from "react";
+import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme();
+
+  const paperTheme = useMemo(
+    () =>
+      colorScheme === "dark"
+        ? { ...MD3DarkTheme, colors: theme.dark }
+        : { ...MD3LightTheme, colors: theme.light },
+    [colorScheme, theme],
+  );
   return (
-    <PaperProvider>
+    <PaperProvider theme={paperTheme}>
       <Stack />
     </PaperProvider>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button>Click Me</Button>
     </View>
   );
 }
